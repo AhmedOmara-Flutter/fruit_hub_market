23315a003773b74f7c6287f07d466ca2c849632c
Ahmed Omara <ahmedhfmo@gmail.com>
2026-04-08 02:39:25 +0200

    add logic to splash to navigate between screen

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -7,11 +7,39 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("home"),
-          centerTitle: true,
+          toolbarHeight: 0.0,
             automaticallyImplyLeading: false,
         ),
+      body: HomeViewBody(),
     );
   }
 }
 
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomAppBar()
+
+      ],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+CircleAvatar(
+  radius: 40,
+  child: SvgPicture.asset(Assets.images.splashCenter.path),
+)
+
+    ],);
+  }
+}
