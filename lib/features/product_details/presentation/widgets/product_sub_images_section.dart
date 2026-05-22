bb844bf90da7c6c2698cadae5a385f0010c9b44c
Ahmed Omara <ahmedhfmo@gmail.com>
2026-05-22 04:40:28 +0300

    add full screen sub images for showing

--- lib/features/product_details/presentation/widgets/product_sub_images_section.dart
+++ lib/features/product_details/presentation/widgets/product_sub_images_section.dart
@@ -14,34 +14,141 @@ class ProductSubImagesSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final images = product.subImages ?? [];
     return SliverToBoxAdapter(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: List.generate(images.length, (index) =>
-            Container(
-              margin: EdgeInsets.only(top: 15, right: 10),
-              height: 100,
-              width: 100,
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.grey.withOpacity(0.2),),),
-            child:CachedNetworkImage(
-              imageUrl: images[index],
-              fit: BoxFit.fill,
-              placeholder: (context, url) =>
+      child: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(images.length, (index) =>
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) =>
+                          _FullScreenGallery(
+                            images: images,
+                            initialIndex: index,
+                          ),
+                    ),
+                  );
+                }, child: Container(
+                margin: EdgeInsets.only(top: 15, right: 10),
+                height: 100,
+                width: 100,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.2),),),
+                child: CachedNetworkImage(
+                  imageUrl: images[index],
+                  fit: BoxFit.fill,
+                  placeholder: (context, url) =>
                   const Center(child: Skeletonizer(child: SizedBox(),)),
-              errorWidget: (context, url, error) =>
-                  Center(child: Center(
-                    child: Icon(
-                      Icons.image_not_supported_outlined,
-                      size: 50,
-                      color: Colors.grey.shade300,),
-                  )),
+                  errorWidget: (context, url, error) =>
+                      Center(child: Center(
+                        child: Icon(
+                          Icons.image_not_supported_outlined,
+                          size: 50,
+                          color: Colors.grey.shade300,),
+                      )),
 
-            ),
-            ),
+                ),
+              ),
+              ),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class _FullScreenGallery extends StatefulWidget {
+  final List<String> images;
+  final int initialIndex;
+
+  const _FullScreenGallery({
+    required this.images,
+    required this.initialIndex,
+  });
+
+  @override
+  State<_FullScreenGallery> createState() => _FullScreenGalleryState();
+}
+
+class _FullScreenGalleryState extends State<_FullScreenGallery> {
+  late PageController controller;
+  late int currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.initialIndex;
+    controller = PageController(initialPage: currentIndex);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: controller,
+            itemCount: widget.images.length,
+            onPageChanged: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Center(
+                child: InteractiveViewer(
+                  child: CachedNetworkImage(
+                    imageUrl: widget.images[index],
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              );
+            },
+          ),
+
+          // زر الإغلاق
+          Positioned(
+            top: 40,
+            left: 20,
+            child: IconButton(
+              icon: const Icon(Icons.close, color: Colors.white, size: 30),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+
+          Positioned(
+            bottom: 30,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                decoration: BoxDecoration(
+                  color: AppColor.mainColor,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Text(
+                  "${currentIndex + 1} / ${widget.images.length}",
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
